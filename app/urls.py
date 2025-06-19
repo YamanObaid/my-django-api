ad0100fed762eb540d958ba283060b6b25648295
Yaman Obaid <yaman6140@gmail.com>
2025-06-19 09:44:21 +0300

    Add DRF and Note model with create endpoint

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello
+from .views import hello, NoteCreateView
 
 urlpatterns = [
     path('', hello),
+    path('create-note/', NoteCreateView.as_view(), name='create-note'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,17 @@
 from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteCreateView(APIView):
+    def post(self, request):
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def hello(request):
     return JsonResponse({"message": "Hello from Render!"})
